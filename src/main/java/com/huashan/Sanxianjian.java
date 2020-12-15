ca3b66bf821dc0a47f3b8e89a800235835283466
linghuchong <983712691@qq.com>
2020-12-15 19:03:34 +0800

    三仙剑

---
+++ src/main/java/com/huashan/Sanxianjian.java
@@ -0,0 +1,10 @@
+package com.huashan;
+
+public class Sanxianjian {
+    public static void main(String[] args) {
+        System.out.println("夺命连环三仙剑");
+        System.out.println("夺命连环三仙剑");
+        System.out.println("夺命连环三仙剑");
+        System.out.println("夺命连环三仙剑");
+    }
+}
